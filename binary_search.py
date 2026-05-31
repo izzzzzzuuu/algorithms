68b779b955478fdad95b3c7c7ba981c5f80ff207
Izzuddin <izzuddinishak8@gmail.com>
2026-06-01 01:41:39 +0800

    basics binary search

---
+++ binary_search.py
@@ -0,0 +1,44 @@
+def binary_search(list,item):
+    low = 0
+    high = len(list)-1
+
+    while low <= high:
+        mid = (low+high)//2
+        guess = list[mid]
+
+        if guess < item:
+            low=mid+1
+        elif guess == item:
+            return mid
+        else:
+            high = mid-1
+    return -1
+
+
+
+
+# my_list=[1,3,5,7,9];
+
+# print binary_search(my_list,3)
+
+# def binary_search(arr, item):
+#     low = 0
+#     high = len(arr) - 1
+
+#     while low <= high:
+#         mid = (low + high) // 2
+#         guess = arr[mid]
+
+#         if guess < item:
+#             low = mid + 1
+#         elif guess == item:
+#             return mid
+#         else:
+#             high = mid - 1
+
+#     return -1
+
+
+my_list = [1, 2, 3, 5, 7, 9]
+
+print(binary_search(my_list, 5))
